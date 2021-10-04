b41e1b679492aacb4fbb675c6a572625ba86a290
KyleDixonVO <KyleDixon@hotmail.ca>
2021-10-04 12:39:18 -0300

    Update Program.cs

--- Conditionals2_Programming1/Program.cs
+++ Conditionals2_Programming1/Program.cs
@@ -8,8 +8,26 @@ namespace Conditionals2_Programming1
 {
     class Program
     {
+        static int score = 0;
+        static int lives = 3; //lives remaining
+        static bool alive = true;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Conditionals 2:");
+
+            lives = lives - 1;
+            if (lives < 0)
+            {
+                lives = 0; //Range check so hud doesn't show -1
+                alive = false;
+            }
+
+            if (lives == 0)
+            {
+                alive = false;
+            }
+            Console.ReadKey(true);
         }
     }
 }
